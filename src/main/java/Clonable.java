a92d8b1dd2f540e708fdafe1f1710ea78ea23340
martaradzi <martaradzi@users.noreply.github.com>
2016-09-23 12:00:56 +0200

    Delete Clonable.java

--- src/main/java/Clonable.java
+++
@@ -1,9 +0,0 @@
-public interface Clonable<E> extends Cloneable {
-
-    /* In order to be able to use clone() everywhere is is overridden with
-       a public version.
-    */
-
-    public E clone();
-
-}
